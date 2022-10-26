00fa56bb257e0113a294ccca00261f1387fe1ec6
Kauan678f <100085923+Kauan678f@users.noreply.github.com>
2022-10-26 17:11:46 -0300

    users2.0
    primeira rota feita

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -3,10 +3,22 @@ var User = require('../models/User')
 class UserController {
     async index(request, response) {
         try{
-            response.json({response: User.findall})
-            response.status(200);
+            var dados = await User.findall()
+            if(dados.status) {
+                if(dados.response > 0) {
+                    response.status(200)
+                    response.json({response: dados.response})
+                }else{
+                    response.status(200)
+                    response.json({response: dados.response})
+                }
+            }else{
+                response.status(500)
+                response.json({err: dados.err})
+            }
         }catch(err){
-            response.err(err)
+            response.status(500)
+            response.json({err: err})
         }
     }
 }
--- models/User.js
+++ models/User.js
@@ -2,8 +2,17 @@ var knex = require("../database/connection");
 
 class User {
     async findall() {
-        var response = await knex.select("*").table("user");
-        return response;
+        try{
+            var response = await knex.select("*").table("alunos");
+            if(response.length > 0) {
+                return {response: response, status: true};
+            }else{
+                return {response: 'alunos Não encontrados', status: true};
+            }
+        }catch(err){
+            return {err: 'erro no servidor, tente novamente', status: false};
+        }
+        
     }
 }
 
